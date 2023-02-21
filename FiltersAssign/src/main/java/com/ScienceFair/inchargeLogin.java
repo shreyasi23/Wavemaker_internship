baafd3cf1f56cf5efb21dd84546ea2c7f904747a
Shreyasi Koppisetti <vasistashreyasi@gmail.com>
2023-02-21 10:44:39 +0530

    Filters assignment

---
+++ FiltersAssign/src/main/java/com/ScienceFair/inchargeLogin.java
@@ -0,0 +1,19 @@
+package com.ScienceFair;
+
+import com.mysql.cj.protocol.Resultset;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+public class inchargeLogin extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("homePage.html");
+        requestDispatcher.forward(req, resp);
+    }
+}
